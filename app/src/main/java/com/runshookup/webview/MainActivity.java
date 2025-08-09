4174b182fabd886ede2c1f2f33741768ecb92a04
contactrunsapp <contactrunsapp@gmail.com>
2025-08-09 03:07:28 +0100

    Create MainActivity.java

---
+++ app/src/main/java/com/runshookup/webview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.runshookup.webview;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://runshookup.online/swipe.php");
+    }
+}
